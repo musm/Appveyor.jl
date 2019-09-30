a384a90336a1c8e53c45ca75530b0e3762bad521
Mustafa M <mus-m@outlook.com>
2019-09-29 23:35:38 -0400

    Update install.ps1

--- bin/install.ps1
+++ bin/install.ps1
@@ -47,8 +47,7 @@ Write-Host "Installing Julia..."
 
 # Install Julia
 if ($julia_version -ge [Version]"1.3") {
-    & $julia_installer /VERYSILENT /DIR=$julia_path
-        echo yes
+    Start-Process -FilePath $julia_installer -ArgumentList "/VERYSILENT /DIR=$julia_path" -NoNewWindow -Wait
 } else {
     Start-Process -FilePath $julia_installer -ArgumentList "/S /D=$julia_path" -NoNewWindow -Wait
 }
